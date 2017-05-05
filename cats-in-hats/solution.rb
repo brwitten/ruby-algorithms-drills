eb53d718e11f3ca34f4b83855fb55eb851a58d24
Jean Weatherwax <jeanmw@users.noreply.github.com>
2017-05-04 20:10:42 -0700

    Update solution.rb

--- cats-in-hats/solution.rb
+++ cats-in-hats/solution.rb
@@ -1,4 +1,31 @@
-def cats_in_hats()
+#this is a helper function to make the cats hash
+def initialize_cats
+    cats = Hash.new(100) #empty hash to hold results
+    i = 1
+    #make all cats with no hats
+    while i <= 100
+        cats[i] = "no hat"
+        i += 1
+    end
+    cats
+end
+
+def cats_in_hats
+    cats = initialize_cats
+    100.times { |x|
+        i = 1
+        while i < cats.length
+            if cats[i + x] == "no hat"
+                cats[i + x] = "hat"
+            elsif cats[i + x] == "hat"
+                cats[i + x] = "no hat"
+            end
+            i += 1
+        end
+    }
+
+    cats.select { |k,v| v == "hat" }
+end
 
 
-cats_in_hats()
+puts cats_in_hats
