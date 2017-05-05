de208f3017cdf63fcb123b6aeaed188273a32153
Jean Weatherwax <jeanmw@users.noreply.github.com>
2017-05-04 20:15:10 -0700

    Update solution.rb

--- cats-in-hats/solution.rb
+++ cats-in-hats/solution.rb
@@ -26,4 +26,24 @@ def cats_in_hats
     cats.select { |k,v| v == "hat" }
 end
 
+# alternate way
+# def cats_in_hats
+#     cats = initialize_cats
+#     counter = 0
+#     while counter < 100 do
+#         i = 1
+#         while i < cats.length
+#             if cats[i + counter] == "no hat"
+#                 cats[i + counter] = "hat"
+#             elsif cats[i + counter] == "hat"
+#                 cats[i + counter] = "no hat"
+#             end
+#             i += 1
+#         end
+#         counter += 1
+#     end
+# 
+#     cats.select { |k,v| v == "hat" }
+# end
+
 puts cats_in_hats
