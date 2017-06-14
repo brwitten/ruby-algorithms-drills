b6c695f0dfa5165fc4c3567a71ca33af45a3148e
Jean Weatherwax <jeanmw@users.noreply.github.com>
2017-06-14 10:22:32 -0700

    Create solution.rb

--- radix-sort/solution.rb
+++ radix-sort/solution.rb
@@ -1,49 +1,49 @@
+
 def radix_sort(array)
-  passes = (array.max == 0) ? 1 : array.max.to_s.length
-  new_list = []
-  passes.times do |i|
-    buckets = make_buckets
-    array.each do |n|
-      digit = get_digit(n, i)
-      buckets[digit] += [n]
-    end
-    new_list, buckets = buckets.flatten, make_buckets
-  end
-  new_list
-end
+	temp = []
+	result = []
 
-#helpers
-def make_buckets
-  Array.new(10,[])
-end
+	array.each do |n|
+		if temp[n] == nil
+			temp[n] = 1
+		else
+			temp[n] = temp[n] + 1
+		end
+	end
 
-def get_digit(n, i)
-  n % (10 ** (i + 1)) / (10 ** i)
+	temp.each_with_index do |n, i|
+		if (n)
+			n.times do
+				result << i
+			end
+		end
+	end
+	result
 end
 
 #alternate solution
 # def radix_sort(array)
-# 	temp = []
-# 	result = []
+#   passes = (array.max == 0) ? 1 : Math.log10(array.max).to_i + 1
+#   new_list = []
+#   passes.times do |i|
+#     buckets = make_buckets
+#     array.each do |n|
+#       digit = get_digit(n, i)
+#       buckets[digit] += [n]
+#     end
+#     new_list, buckets = buckets.flatten, make_buckets
+#   end
+#   new_list
+# end
 #
-# 	array.each do |n|
-# 		if temp[n] == nil
-# 			temp[n] = 1
-# 		else
-# 			temp[n] = temp[n] + 1
-# 		end
-# 	end
+# #helpers
+# def make_buckets
+#   Array.new(10,[])
+# end
 #
-# 	temp.each_with_index do |n, i|
-# 		if (n)
-# 			n.times do
-# 				result << i
-# 			end
-# 		end
-# 	end
-# 	result
+# def get_digit(n, i)
+#   n % (10 ** (i + 1)) / (10 ** i)
 # end
 
 print radix_sort([300,843,1111,23,912])
 print radix_sort([239,40,150,282,60,2,2,120])
-
